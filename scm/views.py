366de4f3076c79f81f1a1d367d9aa0a35f4e3176
Chris Chipman <chchipman@gmail.com>
2024-03-10 17:51:29 -0400

    Simplify requirements.txt, Remove tasks.py, correct views.py

--- scm/views.py
+++ scm/views.py
@@ -17,21 +17,8 @@ def receive_webhook(request):
     driver = SCMManager()
     pipelines = driver.get_pipelines(request)
     driver.trigger_pipeline(pipelines, 'chri')
+    # app.send_task('scm.tasks.task', (1,), {'task': 'ls', 'shell': '/bin/bash'})
+    # app.send_task('scm.tasks.task', ('apply',), {'task': 'terraform', 'shell': '/bin/bash'})
+    # app.send_task('scm.tasks.task', ('inventory_file',), {'task': 'ansible-playbook', 'shell': '/bin/bash'})
     app.send_task('worker.tasks.run', (), {'command': None, 'shell': '/bin/bash'})
-    return HttpResponse('')
-
-
-
-
-
-
-
-@csrf_exempt
-def receive_webhook(request):
-    driver = SCMManager()
-    app.send_task('scm.tasks.task', (1,), {'task': 'ls', 'shell': '/bin/bash'})
-    app.send_task('scm.tasks.task', ('apply',), {'task': 'terraform', 'shell': '/bin/bash'})
-    app.send_task('scm.tasks.task', ('inventory_file',), {'task': 'ansible-playbook', 'shell': '/bin/bash'})
-    #pipelines = driver.get_pipelines(request)
-    #driver.trigger_pipeline(pipelines, 'chri')
     return HttpResponse('')
