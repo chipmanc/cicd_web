337c86c595bb29b6fa2739b919c556ad2e3da061
Chris Chipman <chchipman@gmail.com>
2024-06-02 12:56:35 -0400

    envvars

--- api/serializers.py
+++ api/serializers.py
@@ -28,7 +28,7 @@ class EnvVarSerializer(serializers.ModelSerializer):
 
 class EnvironmentSerializer(serializers.ModelSerializer):
     # env_vars = EnvVarSerializer(required=False)
-    env_vars = serializers.DictField(name=serializers.CharField(), value=serializers.CharField())
+    env_vars = serializers.DictField(child=serializers.CharField())
     project = models.Project
 
     def create(self, validated_data):
