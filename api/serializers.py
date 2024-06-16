d207cd86fcf0259b223f327346c53e7caad15118
Chris Chipman <chchipman@gmail.com>
2024-06-15 21:13:32 -0400

    Add full parameter to mixin

--- api/serializers.py
+++ api/serializers.py
@@ -89,7 +89,7 @@ class PipelineSerializer(serializers.ModelSerializer):
         envs = validated_data.pop('environments', [])
         pipeline = models.Pipeline.objects.create(**validated_data)
         for env in envs:
-            models.Environment.objects.create(name=env)
+            models.Environment.objects.create(name=env, project=pipeline.project)
         return pipeline
 
     def update(self, instance, validated_data):
@@ -101,7 +101,7 @@ class PipelineSerializer(serializers.ModelSerializer):
             instance.environments.all().delete()
             # Add new env_vars
             for env in envs:
-                models.Environment.objects.create(name=env)
+                models.Environment.objects.create(name=env, project=instance.project)
         return instance
 
     class Meta:
